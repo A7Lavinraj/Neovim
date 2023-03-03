3f2eb0edc85a25c37a2ada2b36c2051874bf5127
lavinrajmohan <lavinrajmohan@gmail.com>
2023-03-03 15:28:50 +0530

    finished

--- lua/base/autocmd.lua
+++ lua/base/autocmd.lua
@@ -6,4 +6,9 @@ autocmd FileType cpp nnoremap ;r :w <bar> below 10split term:// g++ %:r.cc -o %:
 autocmd FileType c nnoremap ;r :w <bar> below 10split term:// gcc %:r.c -o %:r.exe && ./%:r.exe<CR>a
 autocmd FileType python nnoremap ;r :w <bar> below 10vsplit term:// python3 %:r.py<CR>a
 augroup END
+augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave * set rnu
+    autocmd BufLeave,FocusLost,InsertEnter * set nornu
+augroup END
 ]])
