28163bb4fa4bf4474fa1130cdf41ec59c6abf142
lavinrajmohan <lavinrajmohan@gmail.com>
2023-03-05 16:16:50 +0530

    cp version finished

--- lua/base/autocmd.lua
+++ lua/base/autocmd.lua
@@ -4,7 +4,7 @@ autocmd!
 autocmd FileType cpp nnoremap ;d :w <bar> below 10split term://bash run.sh %:r<CR>a
 autocmd FileType cpp nnoremap ;r :w <bar> below 10split term:// g++ %:r.cc -o %:r.exe && ./%:r.exe<CR>a
 autocmd FileType c nnoremap ;r :w <bar> below 10split term:// gcc %:r.c -o %:r.exe && ./%:r.exe<CR>a
-autocmd FileType python nnoremap ;r :w <bar> below 10vsplit term:// python3 %:r.py<CR>a
+autocmd FileType python nnoremap ;r :w <bar> below 10split term:// python3 %:r.py<CR>a
 augroup END
 augroup numbertoggle
     autocmd!
--- lua/base/hightlights.lua
+++ lua/base/hightlights.lua
@@ -11,4 +11,5 @@ vim.cmd [[
   autocmd!
   au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
   augroup END
+  colorscheme nightfly
 ]]
